6c9fdb8bbe7e0fe19d785a232025eddf2c97ac61
Tommaso Marzi <marzitommaso@gmail.com>
2022-02-07 13:01:49 +0100

    Update sketch.js

--- random_projects/lorentz/sketch.js
+++ random_projects/lorentz/sketch.js
@@ -16,22 +16,93 @@ var n_trajectory = 100;
 
 var trajectory = [];
 
+let s_inp;
+let r_inp;
+let b_inp;
+
+let x_plot = 'x';
+let y_plot = 'z';
+
+let x_inp;
+let y_inp;
+
+function parameters()
+{  
+  let v_shift = 260;
+  
+  var greeting = createElement('h8', 'rho: ');
+  greeting.position(450, v_shift);
+  r_inp = createInput('28', 'float');
+  r_inp.position(530, v_shift);
+  r_inp.size(70);
+  
+  greeting = createElement('h8', 'sigma: ');
+  greeting.position(450, v_shift+20);
+  s_inp = createInput('10', 'float');
+  s_inp.position(530, v_shift+20);
+  s_inp.size(70);
+  
+  greeting = createElement('h8', 'beta: ');
+  greeting.position(450, v_shift+40);
+  b_inp = createInput('2.66667', 'float');
+  b_inp.position(530, v_shift+40);
+  b_inp.size(70);
+  
+  greeting = createElement('h8', 'X-axis variable: ');
+  greeting.position(420, v_shift+80);
+  x_inp = createInput('x');
+  x_inp.position(530, v_shift+80);
+  x_inp.size(70);
+  
+  greeting = createElement('h8', 'Y-axis variable: ');
+  greeting.position(420, v_shift+100);
+  y_inp = createInput('z');
+  y_inp.position(530, v_shift+100);
+  y_inp.size(70);
+
+}
+
+function get_parameters()
+{
+  beta = b_inp.value();
+  sigma = s_inp.value();
+  rho = r_inp.value();
+  
+  x_plot = x_inp.value();
+  y_plot = y_inp.value();
+}
+
+
 function setup()
 {
   createCanvas(400, 400);
+  parameters();
+  
   for (let i = 0; i < n_trajectory; i++) 
   {
   trajectory[i] = createVector(x,y,z);
   }
 }
 
+function keyReleased() {
+  if(key == ' ')
+  {        
+    get_parameters();
+    background(0);
+    trajectory = [];
+    for (let i = 0; i < n_trajectory; i++) 
+    {
+    trajectory[i] = createVector(x,y,z);
+    }
+  }
+  return false;
+}
 
 function draw()
 {
-  
-  scale(2);
+  scale(3);
   background(0);
-  translate(100,75);
+  translate(50,50);
   
   trajectory[trajectory.length] = createVector(x,y,z);
   
@@ -50,25 +121,64 @@ function draw()
   beginShape();
   for (let i = 0; i < (trajectory.length - n_trajectory - 1); i++)
   {
-      curveVertex(trajectory[i].x,trajectory[i].z);
+      if (x_plot == 'x')
+      {
+        if (y_plot == 'y')
+        {
+          curveVertex(trajectory[i].x,trajectory[i].y);
+        }
+        else
+        {  curveVertex(trajectory[i].x,trajectory[i].z);     
+        }
+      }
+      else
+      {
+        curveVertex(trajectory[i].y,trajectory[i].z); 
+      }
   }
   endShape();
   
   smooth();
   noFill();
   stroke(0,0,255);
-  strokeWeight(4);
+  strokeWeight(2);
   beginShape();
   for (let i = (trajectory.length - n_trajectory - 1); i < trajectory.length; i++)
   {
-      curveVertex(trajectory[i].x,trajectory[i].z);
+      if (x_plot == 'x')
+      {
+        if (y_plot == 'y')
+        {
+          curveVertex(trajectory[i].x,trajectory[i].y);
+        }
+        else
+        {  curveVertex(trajectory[i].x,trajectory[i].z);     
+        }
+      }
+      else
+      {
+        curveVertex(trajectory[i].y,trajectory[i].z); 
+      }
     }
   endShape();
   
   stroke(255,0,0);
-  strokeWeight(8);
-  point(x,z);
+  strokeWeight(4);
+  
+  if (x_plot == 'x')
+    {
+    if (y_plot == 'y')
+    {
+      point(x,y);
+    }
+    else
+    {  
+      point(x,z);     
+    }
+  }
+  else
+  {
+    point(y,z); 
+  }
   
 }
-
-  
